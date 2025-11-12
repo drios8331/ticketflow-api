24bdf36d1137c58d2221672805908dc7c98fa298
drios8331 <73044961+drios8331@users.noreply.github.com>
2025-11-11 22:51:59 -0500

    Inclusion del campo management

--- models/Ticket.js
+++ models/Ticket.js
@@ -4,6 +4,7 @@ const ticketSchema = new mongoose.Schema({
   title: { type: String, required: true },
   namePerson: { type: String, required: true },
   description: { type: String },
+  management: { type: String },
   status: {
     type: String,
     default: "Abierto",
